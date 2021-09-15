3ab486bcf32d9b62e3f5000eb36333588dbac0bd
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-15 17:36:25 +0430

    add categories as GridView add font and styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeals',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: const MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('DeliMeals')),
-      body: const Center(child: Text('Navigation time')),
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              subtitle1: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
+      home: CategoriesScreen(),
     );
   }
 }
