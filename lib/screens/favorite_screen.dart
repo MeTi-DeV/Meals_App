4201f840f349e9caefa29d8468ec005546ef23a8
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-22 23:57:40 +0330

    finish app add favorite screen

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meals.dart';
+import '../widgets/meals_item.dart';
+
 class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+  FavoriteScreen(this.favoriteMeals);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('you can add your favorite meals here'),
-      
+    return Scaffold(
+      body: (favoriteMeals.isEmpty)
+          ? Center(
+              child: Text('you can add your favorite meals here'),
+            )
+          : ListView.builder(
+              itemCount: favoriteMeals.length,
+              itemBuilder: (BuildContext context, int index) {
+                return MealsItem(
+                    id: favoriteMeals[index].id,
+                    title: favoriteMeals[index].title,
+                    ImgUrl: favoriteMeals[index].imageUrl,
+                    duration: favoriteMeals[index].duration,
+                    complexity: favoriteMeals[index].complexity,
+                    affordability: favoriteMeals[index].affordability);
+              },
+            ),
     );
   }
 }
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,16 +2,27 @@ import 'package:flutter/material.dart';
 import './categories_screen.dart';
 import './favorite_screen.dart';
 import '../widgets/main_drawer.dart';
+import '../models/meals.dart';
+
 class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+  TabsScreen(this.favoriteMeals);
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  List<Map<String, dynamic>> _pages = [
-    {'page': CategoriesScreen(), 'title': 'Categories'},
-    {'page': FavoriteScreen(), 'title': 'Favorites'},
-  ];
+  late List<Map<String, dynamic>> _pages;
+
+  @override
+
+  void initState() {
+    _pages = [
+      {'page': CategoriesScreen(), 'title': 'Categories'},
+      {'page': FavoriteScreen(widget.favoriteMeals), 'title': 'Favorites'},
+    ];
+    super.initState();
+  }
   int _selectedPageIndex = 0;
   void _selectPage(int index) {
     setState(() {
@@ -24,22 +35,21 @@ class _TabsScreenState extends State<TabsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title']),
-     
       ),
-      drawer: Drawer(child: MainDrawer(),),
+      drawer: Drawer(
+        child: MainDrawer(),
+      ),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
-      
         unselectedItemColor: Colors.white,
         selectedItemColor: Theme.of(context).accentColor,
         currentIndex: _selectedPageIndex,
-        type:BottomNavigationBarType.shifting ,
+        type: BottomNavigationBarType.shifting,
         onTap: _selectPage,
         items: [
           BottomNavigationBarItem(
-           
               icon: Icon(Icons.category),
-              label:'Categories',
+              label: 'Categories',
               backgroundColor: Theme.of(context).primaryColor),
           BottomNavigationBarItem(
             icon: Icon(Icons.star_outline_outlined),
